adff57ba55f8091068862b0fc8cd787faf86209b
RyszardT <r.tarnowski.77@gmail.com>
2019-06-14 11:09:50 +0200

    Initial commit, testing project transferred to Git.

---
+++ source_code/clx_views.py
@@ -0,0 +1,28 @@
+import datetime
+from flask import render_template, redirect, request
+
+from source_code import app
+from source_code.clx_config import ConfigForm
+
+
+@app.route("/")
+def climatix_root():
+    return render_template("clx_root.html")
+
+
+@app.route("/config", methods=["GET", "POST"])
+def configure_climatix():
+    now = datetime.datetime.now()
+    datetime_format = '%Y-%m-%d %H:%M:%S'
+    formatted_datetime = now.strftime(datetime_format)
+    config_form = ConfigForm()
+    if config_form.validate_on_submit():
+        valid_configuration = config_form.data.copy()
+        valid_configuration.pop("csrf_token")
+        return redirect("/monitor")
+    return render_template("clx_config.html", config_form=ConfigForm(), timestamp=formatted_datetime)
+
+
+@app.route("/monitor", methods=["GET"])
+def monitor_climatix():
+    return render_template("clx_monitor.html")
