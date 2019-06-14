8dc1c0456be19319344eadf06801b3f6a42c3644
RyszardT <r.tarnowski.77@gmail.com>
2019-06-14 14:05:18 +0200

    Commit ???

--- source_code/clx_config.py
+++ source_code/clx_config.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired, Length, IPAddress
 class ConfigForm(FlaskForm):
     ip_address = StringField("IP Address", validators=[DataRequired(), IPAddress(ipv4=True, ipv6=False, message=None)])
     user_name = StringField("User name", validators=[DataRequired()])
-    user_pass = PasswordField("User pass", validators=[DataRequired(), Length(min=4, max=20)])
+    user_pass = PasswordField("User pass", validators=[DataRequired(), Length(min=4, max=15)])
     user_pin = StringField("User PIN", validators=[DataRequired(), Length(min=4, max=4)])
     data_file = FileField("CSV file", validators=[DataRequired()])
     comment = StringField("Comment", validators=[DataRequired()])
