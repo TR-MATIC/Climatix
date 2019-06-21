3466f9d1796e7f763500cbcdc89e9bd3bfaf2ca3
Ryszard Tarnowski <tr-matic@wp.eu>
2019-06-21 16:56:36 +0200

    Basic synchro implemented and tested. OK.

--- source_code/clx_views.py
+++ source_code/clx_views.py
@@ -23,25 +23,35 @@ def configure_climatix():
     sync_file = open("1_datetime.txt", mode="r", encoding="UTF-8")
     formatted_datetime = sync_file.read()
     sync_file.close()
-#    now = datetime.datetime.now()
-#    datetime_format = '%Y-%m-%d %H:%M:%S'
-#    formatted_datetime = now.strftime(datetime_format)
     config_form = ConfigForm()
     if config_form.validate_on_submit():
         valid_configuration = config_form.data.copy()
         valid_configuration.pop("csrf_token")
-        sync_file = open("2_config.txt", mode="w", encoding="UTF-8")
+        conf_file = open("2_config.txt", mode="w", encoding="UTF-8")
         for key in valid_configuration.keys():
-            sync_file.write(valid_configuration[key] + "\n")
-#        sync_file.write(str(valid_configuration))
-        sync_file.close()
+            conf_file.write(valid_configuration[key] + "\n")
+        conf_file.close()
         return redirect("/monitor")
     return render_template("clx_config.html", config_form=ConfigForm(), timestamp=formatted_datetime)
 
 
 @app.route("/monitor", methods=["GET"])
 def monitor_climatix():
-    now = datetime.datetime.now()
-    datetime_format = '%Y-%m-%d %H:%M:%S'
-    formatted_datetime = now.strftime(datetime_format)
-    return render_template("clx_monitor.html", timestamp=formatted_datetime)
+    sync_file = open("1_datetime.txt", mode="r", encoding="UTF-8")
+    formatted_datetime = sync_file.read()
+    sync_file.close()
+
+    clx_configuration = {
+        "ip_address": "",
+        "user_name": "",
+        "user_pass": "",
+        "user_pin": "",
+        "data_file": "",
+        "comment": ""
+    }
+    conf_file = open("2_config.txt", mode="r", encoding="UTF-8")
+    for key in clx_configuration.keys():
+        clx_configuration[key] = conf_file.readline()
+    conf_file.close()
+    #clx_read = requests.request(GET, "192.168.10.101")
+    return render_template("clx_monitor.html", timestamp=formatted_datetime, configuration=clx_configuration)
