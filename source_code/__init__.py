52617aaa8ebbffddf1a25ed452492cacc9b2428c
Ryszard Tarnowski <tr-matic@wp.eu>
2019-06-21 15:37:56 +0200

    Cleanup in __init__ and new file clx_app.py for better structure.

--- source_code/__init__.py
+++ source_code/__init__.py
@@ -2,8 +2,3 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "BFB0DDE2B39686AB4492FACD0F362FB3C7F3424E38C15B0C871E206C969E0175"
-
-from source_code import clx_views
-
-if __name__ == "__main__":
-    app.run(debug=True)
---
+++ source_code/clx_app.py
@@ -0,0 +1,5 @@
+from source_code import app
+from source_code import clx_views
+
+if __name__ == "__main__":
+    app.run(debug=True)
